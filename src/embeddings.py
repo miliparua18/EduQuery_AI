c1f91ac0522034162de1c1f415ba84d26cfd3c1a
Mili <mili.parua292004@gmail.com>
2026-04-28 15:15:52 +0530

    generate embeddings and index chapters in ChromaDB with metadata

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,23 +1,32 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
 import os
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from src.model import get_huggingface_model
+
+load_dotenv()
+
+CHROMA_DB_DIR = "./chroma_db"
 
 
-def get_embeddings_model():
-    return HuggingFaceEmbeddings(model = "sentence-transformers/all-MiniLM-L6-v2")
+#def get_embeddings():
+    #return HuggingFaceEmbeddings(
+        #model = "sentence-transformers/all-MiniLM-L6-v2"
+    #)
 
 
-def built_vector_db(docs):
-    return Chroma.from_documents(
-        documents= docs,
-        embedding=get_embeddings_model(),
-        persist_directory="./chroma_db"
+
+def get_db():
+    return Chroma(
+        persist_directory=CHROMA_DB_DIR,
+        embedding_function=get_huggingface_model()
     )
 
-def get_existing_db():
-    if os.path.exists("./chroma_db"):
-        return Chroma(
-            persist_directory="./chroma_db",
-            embedding_function=get_embeddings_model()
-        )
-    return None
+
+def build_vector_db(docs):
+    db = Chroma.from_documents(
+        documents=docs,
+        embedding=get_huggingface_model(),
+        persist_directory=CHROMA_DB_DIR
+    )
+    return db
