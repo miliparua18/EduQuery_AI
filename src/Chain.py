3ec1be4df618b1b720e729d82955eabca9b4114c
Mili <mili.parua292004@gmail.com>
2026-05-01 12:19:23 +0530

    integrate LangChain RAG chain with tutor-style prompt template

--- src/Chain.py
+++ src/Chain.py
@@ -1,4 +1,3 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
