0cf3402a3214e1f4f74f06279785a7eacc6378ca
Mili <mili.parua292004@gmail.com>
2026-05-04 16:09:18 +0530

    add semantic search with subject and chapter metadata filtering

--- src/retrievers.py
+++ src/retrievers.py
@@ -1,15 +1,40 @@
-def get_filtered_retriever(vector_db, subject=None, chapter=None):
+# def get_filtered_retriever(vector_db, subject=None, chapter=None):
+#     filters = {}
+
+#     if subject and subject != "All":
+#         filters["subject"] = subject
+
+#     if chapter and chapter != "All":
+#         filters["chapter"] = chapter
+
+#     return vector_db.as_retriever(
+#         search_kwargs={
+#             "k": 5,
+#             "filter": filters if filters else None
+#         }
+#     )
+
+
+
+def get_filtered_retriever(vector_db, subject, chapter):
     filters = {}
 
-    if subject and subject != "All":
+    if subject and subject.lower() != "all":
         filters["subject"] = subject
 
-    if chapter and chapter != "All":
+    if chapter:
         filters["chapter"] = chapter
 
-    return vector_db.as_retriever(
-        search_kwargs={
-            "k": 5,
-            "filter": filters if filters else None
-        }
-    )
+    if filters:
+        return vector_db.as_retriever(
+            search_kwargs={
+                "k": 5,
+                "filter": filters
+            }
+        )
+    else:
+        return vector_db.as_retriever(
+            search_kwargs={
+                "k": 5
+            }
+        )
