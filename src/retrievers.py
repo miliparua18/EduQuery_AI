a25da124b9d3dc580a891842dece917ec3e08a20
Mili <mili.parua292004@gmail.com>
2026-05-05 20:57:29 +0530

    add semantic search with subject and chapter metadata filtering

--- src/retrievers.py
+++ src/retrievers.py
@@ -1,21 +1,3 @@
-# def get_filtered_retriever(vector_db, subject=None, chapter=None):
-#     filters = {}
-
-#     if subject and subject != "All":
-#         filters["subject"] = subject
-
-#     if chapter and chapter != "All":
-#         filters["chapter"] = chapter
-
-#     return vector_db.as_retriever(
-#         search_kwargs={
-#             "k": 5,
-#             "filter": filters if filters else None
-#         }
-#     )
-
-
-
 def get_filtered_retriever(vector_db, subject, chapter):
     filters = {}
 
@@ -38,3 +20,40 @@ def get_filtered_retriever(vector_db, subject, chapter):
                 "k": 5
             }
         )
+
+
+
+
+
+# from langchain_classic.retrievers.multi_query import MultiQueryRetriever
+# # from src.model import get_gemini_model
+
+
+
+# # llm = get_gemini_model()
+
+# def get_filtered_retriever(vector_db, subject, chapter, llm=None):
+#     filters = {}
+
+#     if subject:
+#         filters["subject"] = subject
+
+#     if chapter:
+#         filters["chapter"] = chapter
+
+#     # 🔹 Build search kwargs safely
+#     search_kwargs = {"k": 5}
+#     if filters:
+#         search_kwargs["filter"] = filters
+
+#     # 🔹 Create base retriever FIRST
+#     base_retriever = vector_db.as_retriever(
+#         search_type="mmr",   # or "similarity"
+#         search_kwargs=search_kwargs
+#     )
+
+        
+
+#     return base_retriever
+    
+       
